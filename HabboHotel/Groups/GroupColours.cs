24f4c90c7e2d1a6ada2e003f733bcceaee89c325
Mikkel Friis <mikkelbfriis@hotmail.com>
2018-01-06 13:24:11 +0100

    Forgot to save these

--- HabboHotel/Groups/GroupColours.cs
+++ HabboHotel/Groups/GroupColours.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Plus.HabboHotel.Groups
+﻿namespace Plus.HabboHotel.Groups
 {
     public class GroupColours
     {
-        public int Id { get; private set; }
-        public string Colour { get; private set; }
+        public int Id { get; }
+        public string Colour { get; }
 
         public GroupColours(int id, string colour)
         {
-            this.Id = id;
-            this.Colour = colour;
+            Id = id;
+            Colour = colour;
         }
     }
 }
