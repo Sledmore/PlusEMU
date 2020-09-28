af962f39a4991d90fa92abdebd02078ff70a2e8a
AJCollier-1 <72028690+AJCollier-1@users.noreply.github.com>
2020-09-28 15:18:09 +0100

    Update Rotation.cs to use angles
    Will now calculate the angle of Rotation for a better Rotation experience

--- HabboHotel/Rooms/PathFinding/Rotation.cs
+++ HabboHotel/Rooms/PathFinding/Rotation.cs
@@ -5,30 +5,13 @@
         public static int Calculate(int X1, int Y1, int X2, int Y2)
         {
             int Rotation = 0;
+            int dx = X2 - X1;
+            int dy = Y2 - Y1;
 
-            if (X1 > X2 && Y1 > Y2)
-                Rotation = 7;
+            double angle = Math.Atan2( dy, dx );
+            double octant = Math.Round(8*angle / (2*Math.PI)) + 8;
 
-            else if (X1 < X2 && Y1 < Y2)
-                Rotation = 3;
-
-            else if (X1 > X2 && Y1 < Y2)
-                Rotation = 5;
-
-            else if (X1 < X2 && Y1 > Y2)
-                Rotation = 1;
-
-            else if (X1 > X2)
-                Rotation = 6;
-
-            else if (X1 < X2)
-                Rotation = 2;
-
-            else if (Y1 < Y2)
-                Rotation = 4;
-
-            else if (Y1 > Y2)
-                Rotation = 0;
+            Rotation = ((int)octant + 2) % 8;
 
             return Rotation;
         }
